63fe6c0f8e080b7db5b367bb196c3e90c3e9eaae
Alexander Strekanov <strekanov.alexander@gmail.com>
2022-08-30 16:13:42 +0300

    fixed: AtctionsTable component

--- src/components/ActionsTable.js
+++ src/components/ActionsTable.js
@@ -9,77 +9,7 @@ function ActionsTable(props) {
 
     return (
         <Table heading={heading} body={body} />
-        // <table>
-        //     <thead>
-        //         <tr>{columns}</tr>
-        //     </thead>
-        //     <tbody>
-        //         <tr>
-        //             <td>1</td>
-        //             <td>1</td>
-        //             <td>1</td>
-        //             <td>1</td>
-        //             <td>1</td>
-        //             <td>1</td>
-        //             <td>1</td>
-        //             <td>1</td>
-        //         </tr>
-        //     </tbody>
-        // </table>
     )
 }
 
 export default ActionsTable
-
-/*
-import React, { Component } from 'react';
-  
-class App extends Component {
-    render() {
-        var heading = ['Name', 'City', 'Course'];
-        var body =
-            [['Kapil', 'Jaipur', 'MCA'],
-            ['Aakash', 'Hisar', 'Btech'],
-            ['Mani', 'Ranchi', 'MSc'],
-            ['Yash', 'Udaipur', 'Mtech']
-            ];
-        return (
-            <div >
-                <Table heading={heading} body={body} />,
-            </div>
-        );
-    }
-}
-  
-class Table extends Component {
-    render() {
-        var heading = this.props.heading;
-        var body = this.props.body;
-        return (
-            <table style={{ width: 500 }}>
-                <thead>
-                    <tr>
-                        {heading.map(head => <th>{head}</th>)}
-                    </tr>
-                </thead>
-                <tbody>
-                    {body.map(row => <TableRow row={row} />)}
-                </tbody>
-            </table>
-        );
-    }
-}
-  
-class TableRow extends Component {
-    render() {
-        var row = this.props.row;
-        return (
-            <tr>
-                {row.map(val => <td>{val}</td>)}
-            </tr>
-        )
-    }
-}
-  
-export default App;
-*/
